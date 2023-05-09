d6dcb3047135c6aeda0d18ca921c83f5a51076f3
William Yeung <williamyeung1141998@gmail.com>
2023-05-09 17:18:04 -0400

    First commit

---
+++ github-actions-demo/github_actions_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_actions_demoApp.swift
+//  github-actions-demo
+//
+//  Created by William Yeung on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct github_actions_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
